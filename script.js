6dbb0cf1902b2cf5e5736a9a84d25fd0c5daacb8
malakggh <malekgh@ac.sce.ac.il>
2022-09-02 20:08:18 +0300

    start point

---
+++ script.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById("myCanvas");
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+const ctx = canvas.getContext("2d");
+
+const playerImage = new Image();
+playerImage.src = "shadow_dog.png";
+const spriteWidth = 575;
+const spriteHeight = 523
+let frameX = 0;
+let frameY = 5;
+let gameFrame = 0;
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(
+    playerImage,
+    frameX * spriteWidth,
+    frameY * spriteHeight,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight
+  );
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
